92c574600ca4df4b875641ed7f0ee225e1192ebe
SeenSeens <truongtuan829@gmail.com>
2024-06-19 03:28:09 +0700

    #15 cap nhat giao dien

--- app/main/routes/routes.py
+++ app/main/routes/routes.py
@@ -2,20 +2,18 @@ from flask import render_template, request, redirect, url_for, send_from_directo
 from app.main import main
 from app.crawler.crawl_data import  crawl_category
 from app import app
-from app.models import Category
+from app.models import Category, Post
+
 
 # Context processor to make categories available in all templates
 @main.context_processor
 def inject_categories():
     categories = Category.query.all()
     return dict(categories=categories)
-
-
-
-
-
-
-
+@main.context_processor
+def get_recent_posts():
+    recent_posts = Post.query.filter_by(active=True).order_by(Post.created_date.desc()).limit(5).all()
+    return dict(recent_posts=recent_posts)
 @main.route('/media/<path:filename>')
 def media(filename):
     return send_from_directory(app.config['MEDIA_FOLDER'], filename)
